c73a4b5537e22ab56ca3e714dee11f1f3f3f255d
unknown <mohaiminislam94@gmail.com>
2019-10-07 01:33:19 +0600

    setup basic data structures and test route

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const { BlockChain, Block } = require('./server/block-chain');
+console.log(require('./server/block-chain'))
 
 app.get('/', (req, res) => res.send('You are on the root route :D'));
+
+app.get('/blockchain', (req, res) => {
+  let sl0trCoin = new BlockChain();
+  res.send(sl0trCoin.getBlockChain());
+
+});
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
---
+++ server/block-chain.js
@@ -0,0 +1,49 @@
+const SHA256 = require('crypto-js/sha224');
+
+class Block {
+
+  constructor(index,timeStamp, data, prevHash = '') {
+      this.index = index;
+      this.timeStamp = timeStamp;
+      this.data = data;
+      this.prevHash = prevHash;
+      this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    return SHA256(this.index + this.prevHash + this.timeStamp + JSON.stringify(this.data)).toString();
+  }
+
+}
+
+
+class BlockChain {
+
+  constructor() {
+    this.blockChain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, '01/01/0001', 'The Genesis block! D:', 'empty');
+  }
+
+  getBlockChain() {
+    return this.blockChain;
+  }
+
+  getLatestBlock() {
+    return this.blockChain[this.blockChain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.prevHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.blockChain.push(newBlock);
+  }
+
+}
+
+module.exports = {
+  BlockChain,
+  Block
+}
