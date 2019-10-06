354c52521ab24110b6a978c8b69810d85c3c8d79
unknown <mohaiminislam94@gmail.com>
2019-10-07 01:34:24 +0600

    remove un-necessary console log

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const { BlockChain, Block } = require('./server/block-chain');
-console.log(require('./server/block-chain'))
 
 app.get('/', (req, res) => res.send('You are on the root route :D'));
 
