91e3fba0148abc496c0426ea9a846f18b643f224
unknown <mohaiminislam94@gmail.com>
2019-10-13 02:10:20 +0600

    add a block creation route

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app = express();
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 const PORT = 3000;
 const { BlockChain, Block } = require('./server/block-chain');
 
+let mohaCoin = new BlockChain();
+
 app.get('/', (req, res) => res.send('You are on the root route :D'));
 
+app.post('/add-block', (req, res) => {
+  const { body: blockData } = req;
+
+  mohaCoin.addBlock( new Block(blockData))
+
+  if(mohaCoin.getBlockChain().length > 1) {
+    res.send(mohaCoin.getBlockChain());
+  } else {
+    res.send('Block creation failed')
+  }
+})
+
 app.get('/blockchain', (req, res) => {
-  let mohaCoin = new BlockChain();
   res.send(mohaCoin.getBlockChain());
 
 });
