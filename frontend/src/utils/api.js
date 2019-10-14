65a4b0fa9ecc75ec587a0a32e0bb99f37d4a2441
unknown <mohaiminislam94@gmail.com>
2019-10-15 00:01:23 +0600

    connect api root url to heroku backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const url = `http://localhost:4000`;
+// const url = `http://localhost:4000`;
+const url = `https://moha-coin.herokuapp.com`;
 const blockChainUrl = `${url}/blockchain`;
 
 export default {
