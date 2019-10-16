04f64bcac4a9c2ff85010b000fc3c2b32ad9f900
Mohaimin <mohaimin@mohaimin.roar>
2019-10-16 14:51:37 +0600

    remove un-necessary files

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,4 +1,4 @@
 const PORT = 4000;
 const app = require('./app');
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+app.listen(PORT, () => console.log(`MohaCoin is running on: ${PORT}!`));
